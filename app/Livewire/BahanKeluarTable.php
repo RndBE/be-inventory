4ced370c8ce5c05814e8591bc8a3777a3626bd87
WayanDev <wayantrk@gmail.com>
2024-09-25 16:57:39 +0700

    filter data bahan keluar

--- app/Livewire/BahanKeluarTable.php
+++ app/Livewire/BahanKeluarTable.php
@@ -11,12 +11,59 @@ class BahanKeluarTable extends Component
     use WithPagination;
     public $search = "";
     public $perPage = 5;
-    public $id_bahan_keluars, $status;
+    public $id_bahan_keluars, $status,
+    $kode_transaksi, $tgl_keluar, $divisi, $bahanKeluarDetails;
+    public $filter = 'semua';
+    public $totalHarga;
+
+    public function mount()
+    {
+        $this->calculateTotalHarga();
+    }
+
+    public function setFilter($value)
+    {
+        if ($value === 'semua') {
+            $this->filter = null; // Resetting to null to show all
+        } else {
+            $this->filter = $value; // Set the filter for other statuses
+        }
+        $this->resetPage(); // Reset pagination when filter changes
+    }
+
+    public function showBahanKeluar(int $id)
+    {
+        $Data = BahanKeluar::with('bahanKeluarDetails')->findOrFail($id);
+        $this->id_bahan_keluars = $id;
+        $this->tgl_keluar = $Data->tgl_keluar;
+        $this->kode_transaksi = $Data->kode_transaksi;
+        $this->divisi = $Data->divisi;
+        $this->status = $Data->status;
+        $this->bahanKeluarDetails  = $Data->bahanKeluarDetails;
+    }
+
+    public function calculateTotalHarga()
+    {
+        $this->totalHarga = BahanKeluar::where('status', 'Disetujui')->with('bahanKeluarDetails')
+        ->get()
+            ->sum(function ($bahanKeluar) {
+                return $bahanKeluar->bahanKeluarDetails->sum('sub_total');
+            });
+    }
 
     public function render()
     {
         $bahan_keluars = BahanKeluar::with('bahanKeluarDetails')->orderBy('id', 'desc')
-        ->where('tgl_keluar', 'like', '%' . $this->search . '%')
+        ->where('kode_transaksi', 'like', '%' . $this->search . '%')
+            ->when($this->filter === 'Ditolak', function ($query) {
+                return $query->where('status', 'Ditolak');
+            })
+            ->when($this->filter === 'Disetujui', function ($query) {
+                return $query->where('status', 'Disetujui');
+            })
+            ->when($this->filter === 'Belum disetujui', function ($query) {
+                return $query->where('status', 'Belum disetujui');
+            })
             ->paginate($this->perPage);
 
         return view('livewire.bahan-keluar-table', [

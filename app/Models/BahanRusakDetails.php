d7321453ca293fd5ff1bbfacad44881bc8c926f1
WayanDev <wayantrk@gmail.com>
2024-10-02 16:56:10 +0700

    update bahan rusak dari produksi

--- app/Models/BahanRusakDetails.php
+++ app/Models/BahanRusakDetails.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BahanRusak extends Model
+class BahanRusakDetails extends Model
 {
     use HasFactory;
 

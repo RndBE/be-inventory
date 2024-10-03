7442e30f2a36bef0ebfd4d07b371d078852f377a
WayanDev <wayantrk@gmail.com>
2024-10-03 16:23:35 +0700

    model bahan setengahjadi dan bahan setengahjadi details

---
+++ app/Models/BahanSetengahjadiDetails.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BahanSetengahjadiDetails extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bahan_setengahjadi_details';
+    protected $guarded = [];
+
+    public function bahanSetengahjadi()
+    {
+        return $this->belongsTo(BahanSetengahjadi::class);
+    }
+
+    public function dataBahan()
+    {
+        return $this->belongsTo(Bahan::class, 'bahan_id');
+    }
+}

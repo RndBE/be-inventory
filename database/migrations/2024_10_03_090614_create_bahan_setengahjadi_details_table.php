fa99c1449965b34513b569232760292f16df8574
WayanDev <wayantrk@gmail.com>
2024-10-03 16:23:01 +0700

    tabel bahan setengah jadi dan bahan setengah jadi details

---
+++ database/migrations/2024_10_03_090614_create_bahan_setengahjadi_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bahan_setengahjadi_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bahan_setengahjadi_id')->constrained('bahan_setengahjadis')->onDelete('cascade');
+            $table->foreignId('bahan_id')->constrained('bahan');
+            $table->integer('qty');
+            $table->integer('sisa');
+            $table->integer('unit_price');
+            $table->integer('sub_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bahan_setengahjadi_details');
+    }
+};

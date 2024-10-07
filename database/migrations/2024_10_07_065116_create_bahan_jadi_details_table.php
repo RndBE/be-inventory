5657c595f01de8ec10305790cf0c11bedfda86d7
WayanDev <wayantrk@gmail.com>
2024-10-07 14:09:37 +0700

    create table and model bahan jadi dan bahan jadi setails

---
+++ database/migrations/2024_10_07_065116_create_bahan_jadi_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bahan_jadi_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bahan_jadi_id')->constrained('bahan_jadis')->onDelete('cascade');
+            $table->string('nama_produk');
+            $table->string('gambar');
+            $table->integer('qty');
+            $table->integer('sisa');
+            $table->foreignId('unit_id')->constrained('unit');
+            $table->integer('unit_price');
+            $table->integer('sub_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bahan_jadi_details');
+    }
+};

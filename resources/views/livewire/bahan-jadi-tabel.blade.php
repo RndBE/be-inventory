375a4b42743cfb0def3fa02be38d65704f6a36f0
WayanDev <wayantrk@gmail.com>
2024-10-10 10:36:42 +0700

    Panggil nama bahan di tabel bahan jadi

--- resources/views/livewire/bahan-jadi-tabel.blade.php
+++ resources/views/livewire/bahan-jadi-tabel.blade.php
@@ -35,7 +35,7 @@
                     @if($bahanJadi->bahanJadiDetails->isNotEmpty())
                         @foreach($bahanJadi->bahanJadiDetails as $detail)
                             <div>
-                                {{ $detail->nama_produk }} | <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">{{ $detail->sisa }}</span>
+                                {{ $detail->dataBahan->nama_bahan }} | <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">{{ $detail->sisa }} {{ $detail->dataBahan->dataUnit->nama }}</span>
                             </div>
                         @endforeach
                     @else

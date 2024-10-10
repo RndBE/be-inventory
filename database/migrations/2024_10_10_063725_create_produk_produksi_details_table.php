7422b314549331d46050be4f6877982bdc9a0d2a
WayanDev <wayantrk@gmail.com>
2024-10-10 13:50:17 +0700

    add model and table migration produk produksi detail

---
+++ database/migrations/2024_10_10_063725_create_produk_produksi_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk_produksi_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produk_produksis_id')->constrained('produk_produksis')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('bahan_id')->constrained('bahan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk_produksi_details');
+    }
+};

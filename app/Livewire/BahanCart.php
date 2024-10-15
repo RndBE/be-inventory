c3e53bea31b73dd83e85a33a3cfcaac4140e9404
WayanDev <wayantrk@gmail.com>
2024-10-15 08:31:49 +0700

    Update produk produksi

--- app/Livewire/BahanCart.php
+++ app/Livewire/BahanCart.php
@@ -9,31 +9,27 @@ use Livewire\Component;
 class BahanCart extends Component
 {
     public $cart = [];
-    public $produkProduksisId; // untuk menangkap id produk produksi dalam mode edit
+    public $produkProduksisId;
 
     protected $listeners = ['bahanSelected' => 'addToCart'];
 
     public function mount($produkProduksisId = null)
     {
-        // Jika dalam mode edit, ambil bahan terkait produk produksi
         $this->produkProduksisId = $produkProduksisId;
 
         if ($this->produkProduksisId) {
             $this->loadCartItems();
         } else {
-            // Inisialisasi keranjang dari sesi atau mulai dengan array kosong
             $this->cart = session()->get('cart', []);
         }
     }
 
     public function loadCartItems()
     {
-        // Ambil bahan terkait produk produksi dari database
         $details = ProdukProduksiDetail::where('produk_produksis_id', $this->produkProduksisId)
             ->with('dataBahan')
             ->get();
 
-        // Konversi ke format yang sesuai untuk keranjang
         foreach ($details as $detail) {
             $this->cart[] = [
                 'id' => $detail->dataBahan->id,
@@ -44,27 +40,21 @@ class BahanCart extends Component
 
     public function addToCart($bahan)
     {
-        // Konversi objek menjadi array
         $bahan = (array) $bahan;
 
-        // Periksa apakah bahan sudah ada di keranjang
         if (!collect($this->cart)->contains('id', $bahan['id'])) {
-            // Tambahkan bahan ke keranjang
             $this->cart[] = $bahan;
 
-            // Simpan ke sesi
             session()->put('cart', $this->cart);
         }
     }
 
     public function removeItem($bahanId)
     {
-        // Filter menggunakan koleksi
         $this->cart = collect($this->cart)->reject(function ($item) use ($bahanId) {
             return isset($item['id']) && $item['id'] == $bahanId;
-        })->toArray();  // Konversi kembali ke array setelah reject
+        })->toArray();
 
-        // Simpan pembaruan ke sesi
         session()->put('cart', $this->cart);
     }
 
@@ -75,4 +65,5 @@ class BahanCart extends Component
         ]);
     }
 }
+
 

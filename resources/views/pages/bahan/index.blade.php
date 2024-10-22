4ec195da98d25a1a1a78c7d0c8cf3148c81b648b
WayanDev <wayantrk@gmail.com>
2024-10-22 18:50:13 +0700

    update bahan-table

--- resources/views/pages/bahan/index.blade.php
+++ resources/views/pages/bahan/index.blade.php
@@ -1,6 +1,4 @@
 @section('title', 'Bahan | BE INVENTORY')
 <x-app-layout>
-    <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        @livewire('bahan-table')
-    </div>
+    @livewire('bahan-table')
 </x-app-layout>

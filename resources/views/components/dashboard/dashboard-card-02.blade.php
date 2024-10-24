6704b22374440769f78eefea2b4a08f7712645ad
WayanDev <wayantrk@gmail.com>
2024-10-24 11:22:17 +0700

    up

--- resources/views/components/dashboard/dashboard-card-02.blade.php
+++ resources/views/components/dashboard/dashboard-card-02.blade.php
@@ -1,14 +1,60 @@
 <div class="relative flex flex-col col-span-full sm:col-span-6 xl:col-span-3 bg-white dark:bg-gray-800 shadow-md rounded-xl">
-        <div class="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr bg-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-list"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l11 0" /><path d="M9 12l11 0" /><path d="M9 18l11 0" /><path d="M5 6l0 .01" /><path d="M5 12l0 .01" /><path d="M5 18l0 .01" /></svg>
-        </div>
-        <div class="p-4 text-right">
-            <div class="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">Sales</div>
+    <div class="px-5 pt-5">
+        <header class="flex justify-between items-start mb-2">
+            <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-100">Acme Advanced</h2>
+            <!-- Menu button -->
+            <div class="relative inline-flex" x-data="{ open: false }">
+                <button
+                    class="rounded-full"
+                    :class="open ? 'bg-gray-100 dark:bg-gray-700/60 text-gray-500 dark:text-gray-400': 'text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400'"
+                    aria-haspopup="true"
+                    @click.prevent="open = !open"
+                    :aria-expanded="open"
+                >
+                    <span class="sr-only">Menu</span>
+                    <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32">
+                        <circle cx="16" cy="16" r="2" />
+                        <circle cx="10" cy="16" r="2" />
+                        <circle cx="22" cy="16" r="2" />
+                    </svg>
+                </button>
+                <div
+                    class="origin-top-right z-10 absolute top-full right-0 min-w-36 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700/60 py-1.5 rounded-lg shadow-lg overflow-hidden mt-1"
+                    @click.outside="open = false"
+                    @keydown.escape.window="open = false"
+                    x-show="open"
+                    x-transition:enter="transition ease-out duration-200 transform"
+                    x-transition:enter-start="opacity-0 -translate-y-2"
+                    x-transition:enter-end="opacity-100 translate-y-0"
+                    x-transition:leave="transition ease-out duration-200"
+                    x-transition:leave-start="opacity-100"
+                    x-transition:leave-end="opacity-0"
+                    x-cloak
+                >
+                    <ul>
+                        <li>
+                            <a class="font-medium text-sm text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 flex py-1 px-3" href="#0" @click="open = false" @focus="open = true" @focusout="open = false">Option 1</a>
+                        </li>
+                        <li>
+                            <a class="font-medium text-sm text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200 flex py-1 px-3" href="#0" @click="open = false" @focus="open = true" @focusout="open = false">Option 2</a>
+                        </li>
+                        <li>
+                            <a class="font-medium text-sm text-red-500 hover:text-red-600 flex py-1 px-3" href="#0" @click="open = false" @focus="open = true" @focusout="open = false">Remove</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="text-xs font-semibold text-gray-400 dark:text-gray-500 uppercase mb-1">Sales</div>
+        <div class="flex items-start">
             <div class="text-3xl font-bold text-gray-800 dark:text-gray-100 mr-2">${{ number_format($dataFeed->sumDataSet(2, 1), 0) }}</div>
+            <div class="text-sm font-medium text-red-700 px-1.5 bg-red-500/20 rounded-full">-14%</div>
         </div>
-        <div class="border-t border-blue-gray-50 p-4">
-            <p class="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-                <strong class="text-green-500"></strong>&nbsp;
-            </p>
-        </div>
+    </div>
+    <!-- Chart built with Chart.js 3 -->
+    <!-- Check out src/js/components/dashboard-card-02.js for config -->
+    <div class="grow max-sm:max-h-[128px] xl:max-h-[128px]">
+        <!-- Change the height attribute to adjust the chart height -->
+        <canvas id="dashboard-card-02" width="389" height="128"></canvas>
+    </div>
 </div>

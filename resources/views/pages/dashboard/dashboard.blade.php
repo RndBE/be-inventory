af7234a9e9f2caafc8dd713d09eec97fee9fed30
WayanDev <wayantrk@gmail.com>
2024-11-04 09:41:44 +0700

    update

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -101,16 +101,6 @@
                 </div>
             </div>
 
-
-            <div class="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
-                <header class="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
-                    <h2 class="font-semibold text-gray-800 dark:text-gray-100">Bahan Masuk vs Bahan Keluar</h2>
-                </header>
-                <div class="grow">
-                    <canvas id="data-bahan-masuk" width="595" height="248"></canvas>
-                </div>
-            </div>
-
             <div class="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
                 <header class="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
                     <h2 class="font-semibold text-gray-800 dark:text-gray-100">Bahan Masuk vs Bahan Keluar</h2>

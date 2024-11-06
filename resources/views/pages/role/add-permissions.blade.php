3aea3387461e5eb451c8bbe64584eda945afafc6
WayanDev <wayantrk@gmail.com>
2024-11-06 15:24:10 +0700

    update

--- resources/views/pages/role/add-permissions.blade.php
+++ resources/views/pages/role/add-permissions.blade.php
@@ -92,27 +92,31 @@
                         <div class="border-gray-900/10 pb-12">
                             <h6 class="p-4 text-2xl text-gray-800 dark:text-gray-100 font-bold">Role : {{ $role->name }}</h6>
 
-                            @foreach ($permissions as $category => $permissionGroup) <!-- Mengelompokkan berdasarkan kategori -->
-                                <div class="p-4 flex flex-wrap space-x-2">
-                                    <h4 class="p-4 text-lg font-semibold text-gray-600 dark:text-gray-300">{{ $category }}</h4> <!-- Nama kategori -->
+                            @foreach ($permissions as $category => $permissionGroup)
+    <div class="p-4">
+        <!-- Display the category name -->
+        <h4 class="text-lg font-semibold text-gray-600 dark:text-gray-300">{{ $category }}</h4>
 
-                                    <div class="p-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4">
-                                        @foreach ($permissionGroup as $permission)
-                                            <div class="col-md-2">
-                                                <label>
-                                                    <input
-                                                        type="checkbox"
-                                                        name="permission[]"
-                                                        value="{{ $permission->name }}"
-                                                        {{ in_array($permission->id, $rolePermissions) ? 'checked':'' }}
-                                                    />
-                                                    {{ $permission->name }}
-                                                </label>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            @endforeach
+        <!-- Use a flex container to handle the layout -->
+        <div class="flex flex-wrap gap-4">
+            @foreach ($permissionGroup as $permission)
+                <div class="flex items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
+                    <label class="flex items-center space-x-2">
+                        <input
+                            type="checkbox"
+                            name="permission[]"
+                            value="{{ $permission->name }}"
+                            {{ in_array($permission->id, $rolePermissions) ? 'checked' : '' }}
+                            class="form-checkbox"
+                        />
+                        <span>{{ $permission->name }}</span>
+                    </label>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endforeach
+
                         </div>
                     </div>
                 </form>

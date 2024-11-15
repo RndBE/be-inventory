57271144266ff8f98da6bcd74cf86a583d66933e
WayanDev <wayantrk@gmail.com>
2024-11-15 14:14:27 +0700

    update permission administrasi

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -58,7 +58,7 @@
             </div>
             <!-- Master Data -->
             <div>
-                @role('superadmin|purchasing|produksi')
+                @role('superadmin|purchasing|produksi|administrasi')
                 <h3 class="text-xs uppercase text-gray-400 dark:text-gray-500 font-semibold pl-3">
                     <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
                     <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Data</span>
@@ -66,7 +66,7 @@
                 @endrole
                 <ul class="mt-3">
                     <!-- Data Master -->
-                    @role('superadmin|purchasing|produksi')
+                    @role('superadmin|purchasing|produksi|administrasi')
                     <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-[linear-gradient(135deg,var(--tw-gradient-stops))] @if(in_array(Request::segment(1), ['datamaster'])){{ 'from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04]' }}@endif" x-data="{ open: {{ in_array(Request::segment(1), ['datamaster']) ? 1 : 0 }} }">
                         <a class="block text-gray-800 dark:text-gray-100 truncate transition @if(!in_array(Request::segment(1), ['datamaster'])){{ 'hover:text-gray-900 dark:hover:text-white' }}@endif" href="#0" @click.prevent="open = !open; sidebarExpanded = true">
                             <div class="flex items-center justify-between">
@@ -128,7 +128,7 @@
                     </li>
                     @endrole
                     <!-- Transaksi -->
-                    @role('superadmin|purchasing')
+                    @role('superadmin|purchasing|administrasi')
                     <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 bg-[linear-gradient(135deg,var(--tw-gradient-stops))] @if(in_array(Request::segment(1), ['transaksi'])){{ 'from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04]' }}@endif" x-data="{ open: {{ in_array(Request::segment(1), ['transaksi']) ? 1 : 0 }} }">
                         <a class="block text-gray-800 dark:text-gray-100 truncate transition @if(!in_array(Request::segment(1), ['transaksi'])){{ 'hover:text-gray-900 dark:hover:text-white' }}@endif" href="#0" @click.prevent="open = !open; sidebarExpanded = true">
                             <div class="flex items-center justify-between">
@@ -241,7 +241,7 @@
                 </ul>
             </div>
             <!-- Produksi -->
-            @role('superadmin|produksi|purchasing')
+            @role('superadmin|produksi|purchasing|administrasi')
             <div>
                 <h3 class="text-xs uppercase text-gray-400 dark:text-gray-500 font-semibold pl-3">
                     <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>
@@ -291,7 +291,7 @@
             </div>
             @endrole
             <!-- RnD -->
-            @role('superadmin|rnd')
+            @role('superadmin|rnd|administrasi')
             <div>
                 <h3 class="text-xs uppercase text-gray-400 dark:text-gray-500 font-semibold pl-3">
                     <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span>

63e1bbb70c56e68c499ec8237dee0c9d48ebd3c9
WayanDev <wayantrk@gmail.com>
2025-01-10 11:32:19 +0700

    modal purchases, pembelian bahan

--- resources/views/pages/pembelian-bahan/remove.blade.php
+++ resources/views/pages/pembelian-bahan/remove.blade.php
@@ -1,8 +1,26 @@
-@if ($isDeleteModalOpen)
-<div wire:ignore.self id="deletepembelianbahan-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 flex items-center justify-center z-50 w-full h-full bg-black bg-opacity-50" wire:click.self="closeModal">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+{{-- @if ($isDeleteModalOpen) --}}
+<div x-data="{ isOpen: @entangle('isDeleteModalOpen') }"
+    x-show="isOpen"
+    class="fixed inset-0 flex items-center justify-center z-50 w-full h-full"
+    style="background-color: rgba(0, 0, 0, 0.5); backdrop-filter: blur(5px);"
+    @keydown.escape.window="isOpen = false; $wire.closeModal();"
+    x-transition:enter="transition ease-out duration-900"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-900"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0">
+    <div class="relative p-4 w-full max-w-md max-h-full" x-show="isOpen"
+        @click.outside="isOpen = false; $wire.closeModal();"
+        x-transition:enter="transition ease-out duration-900 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-900 transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95">
+
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button wire:click="closeModal" type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="deletepembelianbahan-modal">
+            <button wire:click="closeModal" type="button" @click="isOpen = false; $wire.closeModal();" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -12,7 +30,7 @@
                 <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah Anda yakin ingin menghapus bahan?</h3>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah Anda yakin ingin menghapus pembelian bahan?</h3>
                 <div class="flex justify-center gap-4">
                     <form action="{{ route('pengajuan-pembelian-bahan.destroy', (int)$id_pembelian_bahan) }}" method="POST" class="inline-flex">
                         @csrf
@@ -29,4 +47,4 @@
         </div>
     </div>
 </div>
-@endif
+{{-- @endif --}}
--- resources/views/pages/pembelian-bahan/test.blade.php
+++ resources/views/pages/pembelian-bahan/test.blade.php
@@ -1,20 +1,45 @@
 <!-- Main modal -->
-@if ($isShowModalOpen)
-<div wire:ignore.self id="showpembelianbahan-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 flex items-center justify-center z-50 w-full h-full bg-black bg-opacity-50" wire:click.self="closeModal">
-    <div class="relative p-4 pt-12 w-full max-w-md max-h-full">
+{{-- @if ($isShowModalOpen) --}}
+<div
+    x-data="{ isOpen: @entangle('isShowModalOpen') }"
+    x-show="isOpen"
+    class="fixed inset-0 flex items-center justify-center z-50 w-full h-full"
+    style="background-color: rgba(0, 0, 0, 0.5); backdrop-filter: blur(5px);"
+    @keydown.escape.window="isOpen = false; $wire.closeModal();"
+    x-transition:enter="transition ease-out duration-900"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-900"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0"
+>
+    <div
+        class="relative p-2 pt-2 w-full max-w-md bg-white rounded-lg shadow dark:bg-gray-700 overflow-y-auto"
+        style="max-height: 80vh;"
+        x-show="isOpen"
+        @click.outside="isOpen = false; $wire.closeModal();"
+        x-transition:enter="transition ease-out duration-900 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-900 transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95"
+    >
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-2 dark:border-gray-600">
-                <button wire:click="closeModal" type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="showpembelianbahan-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
+        <div class="flex items-center justify-between p-2 dark:border-gray-600">
+            <button
+                type="button"
+                @click="isOpen = false; $wire.closeModal();"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+        </div>
             <!-- Modal body -->
-            <div class="p-6 max-h-[75vh] overflow-y-auto">
+            {{-- <div class="p-6 max-h-[75vh] overflow-y-auto"> --}}
                 @if($jenis_pengajuan === 'Pembelian Bahan/Barang/Alat Lokal')
                     <div class="flex w-full items-center justify-center">
                         <div class="w-[350px] rounded bg-gray-50 px-6 m-4 shadow-lg">
@@ -436,8 +461,8 @@
                         </div>
                     </div>
                 @endif
-            </div>
+            {{-- </div> --}}
         </div>
     </div>
 </div>
-@endif
+{{-- @endif --}}

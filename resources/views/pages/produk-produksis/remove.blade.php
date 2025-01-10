1566be5c7052343ae117eb276c92696827da9f90
WayanDev <wayantrk@gmail.com>
2025-01-10 11:04:34 +0700

    modal pada fitur, jenis bahan, unit, supplier, produk produksi dan kontrak

--- resources/views/pages/produk-produksis/remove.blade.php
+++ resources/views/pages/produk-produksis/remove.blade.php
@@ -1,12 +1,32 @@
-@if($isDeleteModalOpen)
-<div wire:ignore.self id="deleteprodukproduksis-modal" tabindex="-1" class="fixed inset-0 flex items-center justify-center z-50 w-full h-full bg-black bg-opacity-50" wire:click.self="closeModal">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+{{-- @if($isDeleteModalOpen) --}}
+<div x-data="{ isOpen: @entangle('isDeleteModalOpen') }"
+    x-show="isOpen"
+    class="fixed inset-0 flex items-center justify-center z-50 w-full h-full"
+    style="background-color: rgba(0, 0, 0, 0.5); backdrop-filter: blur(5px);"
+    @keydown.escape.window="isOpen = false; $wire.closeModal();"
+    x-transition:enter="transition ease-out duration-900"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-900"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0">
+
+    <div class="relative p-4 w-full max-w-md max-h-full"
+        x-show="isOpen"
+        @click.outside="isOpen = false; $wire.closeModal();"
+        x-transition:enter="transition ease-out duration-900 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-900 transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95">
+
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button wire:click="closeModal" type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="deleteprodukproduksis-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">Close modal</span>
+            <button wire:click="closeModal" type="button" @click="isOpen = false; $wire.closeModal();" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
             </button>
             <div class="p-4 md:p-5 text-center">
                 <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -29,4 +49,4 @@
         </div>
     </div>
 </div>
-@endif
+{{-- @endif --}}
--- resources/views/pages/supplier/edit.blade.php
+++ resources/views/pages/supplier/edit.blade.php
@@ -1,7 +1,26 @@
 <!-- Main modal -->
-@if($isEditModalOpen)
-<div wire:ignore.self id="editsupplier-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 flex items-center justify-center z-50 w-full h-full bg-black bg-opacity-50" wire:click.self="closeModal">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+{{-- @if($isEditModalOpen) --}}
+<div x-data="{ isOpen: @entangle('isEditModalOpen') }"
+    x-show="isOpen"
+    class="fixed inset-0 flex items-center justify-center z-50 w-full h-full"
+    style="background-color: rgba(0, 0, 0, 0.5); backdrop-filter: blur(5px);"
+    @keydown.escape.window="isOpen = false; $wire.closeModal();"
+    x-transition:enter="transition ease-out duration-900"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-900"
+    x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0">
+
+    <div class="relative p-4 w-full max-w-md max-h-full"
+        x-show="isOpen"
+        @click.outside="isOpen = false; $wire.closeModal();"
+        x-transition:enter="transition ease-out duration-900 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-900 transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
@@ -9,7 +28,7 @@
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                     Edit Supplier
                 </h3>
-                <button wire:click="closeModal" type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="editsupplier-modal">
+                <button wire:click="closeModal" type="button" @click="isOpen = false; $wire.closeModal();" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -32,4 +51,4 @@
         </div>
     </div>
 </div>
-@endif
+{{-- @endif --}}

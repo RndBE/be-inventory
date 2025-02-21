cb8e29a5e0784dde1e32928f07c9cfe42a1fa41f
WayanDev <wayantrk@gmail.com>
2025-02-21 15:30:44 +0700

    update

--- app/Livewire/PengajuanModal.php
+++ app/Livewire/PengajuanModal.php
@@ -3,20 +3,10 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use Illuminate\Support\Facades\Session;
 
 class PengajuanModal extends Component
 {
-    public $showModal = false;
-
-    public function mount()
-    {
-        // Cek apakah session 'modal_shown' sudah ada
-        if (!Session::has('modal_shown')) {
-            $this->showModal = true;
-            Session::put('modal_shown', true); // Tandai modal sudah pernah ditampilkan
-        }
-    }
+    public $showModal = true;
 
     public function closeModal()
     {

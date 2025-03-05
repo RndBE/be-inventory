5c2476d83d1c8ae87a0da4e16d4931408554ea34
WayanDev <wayantrk@gmail.com>
2025-03-05 20:59:26 +0700

    add kolom table

---
+++ database/migrations/2025_03_03_012645_add_produk_id_to_projek_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projek_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('produk_id')->nullable()->after('bahan_id');
+
+            $table->foreign('produk_id')->references('id')->on('bahan_setengahjadi_details')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projek_details', function (Blueprint $table) {
+            $table->dropForeign(['produk_id']);
+            $table->dropColumn('produk_id');
+        });
+    }
+};
---
+++ database/migrations/2025_03_03_085202_add_produk_id_to_bahan_retur_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bahan_retur_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('produk_id')->nullable()->after('bahan_id');
+            $table->string('serial_number')->nullable();
+
+            $table->foreign('produk_id')->references('id')->on('bahan_setengahjadi_details')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bahan_retur_details', function (Blueprint $table) {
+            $table->dropForeign(['produk_id']);
+            $table->dropColumn('produk_id');
+            $table->dropColumn('serial_number');
+        });
+    }
+};

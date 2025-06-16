260cbc36550d6ad4ae5d2d297e02d05eb50c271a
RndBE <rnd.software.be@gmail.com>
2025-06-16 11:36:40 +0700

    update selisih

--- app/Http/Controllers/StockOpnameController.php
+++ app/Http/Controllers/StockOpnameController.php
@@ -682,7 +682,7 @@ class StockOpnameController extends Controller
             ]);
 
             foreach ($stockOpname->stockOpnameDetails as $detail) {
-                $selisih = $detail->selisih_audit;
+                $selisih = $detail->selisih;
 
                 // Abaikan jika selisih positif
                 if ($selisih > 0) continue;

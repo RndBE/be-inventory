88b758f06865fa844edd844d5365dc73286f514c
RndBE <rnd.software.be@gmail.com>
2025-06-19 16:08:06 +0700

    update edit proyek, tambah bahan dengan koma

--- app/Http/Controllers/ProjekController.php
+++ app/Http/Controllers/ProjekController.php
@@ -375,10 +375,10 @@ class ProjekController extends Controller
                     ];
                 }
 
-                $groupedItems[$key]['qty'] += (int) $item['qty'];
-                $groupedItems[$key]['jml_bahan'] += (int) $item['jml_bahan'];
-                $groupedItems[$key]['sub_total'] += (float) $item['sub_total'];
-                $totalQty += (int) $item['qty']; // Tambahkan qty item ke total qty
+                $groupedItems[$key]['qty'] += $item['qty'];
+                $groupedItems[$key]['jml_bahan'] += $item['jml_bahan'];
+                $groupedItems[$key]['sub_total'] += $item['sub_total'];
+                $totalQty += $item['qty']; // Tambahkan qty item ke total qty
             }
 
             if ($totalQty !== 0) {

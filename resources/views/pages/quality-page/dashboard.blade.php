c96b5ac8a5364a50cce9eb4084f07ac57bbea60d
RndBE <rnd.software.be@gmail.com>
2025-08-15 13:40:42 +0700

    update

---
+++ resources/views/pages/quality-page/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-quality-layout title="Dashboard QC"> {{-- Memanggil komponen dan passing judul --}}
+    <h1>Dashboard wseafasfaf fgbsfdhg sss fdgs sadasd wedfwe QC</h1>
+</x-quality-layout>
---
+++ resources/views/pages/quality-page/qc-bahan-masuk/create.blade.php
@@ -0,0 +1,4 @@
+<x-quality-layout title="Tambah QC Bahan Masuk">
+    {{-- @livewire('qc-bahan-masuk-tambah') --}}
+    @livewire('qc-wizard')
+</x-quality-layout>

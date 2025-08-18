4f1c845b8884ec75e7ffe2172b8564558a2fd5bb
RndBE <rnd.software.be@gmail.com>
2025-08-18 09:35:38 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
       input: [
         'resources/css/app.css',
         'resources/js/app.js',
-        'resources/assets/css/app.css',
-        'resources/assets/js/app.js',
+        // 'resources/assets/css/app.css',
+        // 'resources/assets/js/app.js',
       ],
       refresh: true,
     }),

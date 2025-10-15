20e8a28fa325f3a8117441ead532da47c01fe9b8
RndBE <rnd.software.be@gmail.com>
2025-10-15 13:34:51 +0700

    update font halaman login

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -13,9 +13,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        * {
+            font-family: 'Lexend Deca', sans-serif !important;
+        }
+    </style>
 
     <!-- Styles -->
     @livewireStyles

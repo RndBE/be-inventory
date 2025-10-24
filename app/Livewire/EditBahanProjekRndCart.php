3efbe0741cb7a4f9f1485b70f92abe762f138217
RndBE <rnd.software.be@gmail.com>
2025-10-24 15:07:59 +0700

    update edit bahan projek rnd cart

--- app/Livewire/EditBahanProjekRndCart.php
+++ app/Livewire/EditBahanProjekRndCart.php
@@ -658,7 +658,8 @@ class EditBahanProjekRndCart extends Component
                 'serial_number' => $rusak['serial_number'] ?? null,
                 'qty' => $rusak['qty'] ?? 0,
                 'unit_price' => $rusak['unit_price'] ?? 0,
-                'sub_total' => ($rusak['qty'] ?? 0) * ($rusak['unit_price'] ?? 0),
+                // 'sub_total' => ($rusak['qty'] ?? 0) * ($rusak['unit_price'] ?? 0),
+                'sub_total' => floatval(str_replace(',', '.', $rusak['qty'] ?? 0)) * floatval(str_replace(',', '.', $rusak['unit_price'] ?? 0)),
             ];
         }
 
@@ -685,7 +686,8 @@ class EditBahanProjekRndCart extends Component
                 'serial_number' => $retur['serial_number'] ?? null,
                 'qty' => $retur['qty'] ?? 0,
                 'unit_price' => $retur['unit_price'] ?? 0,
-                'sub_total' => ($retur['qty'] ?? 0) * ($retur['unit_price'] ?? 0),
+                // 'sub_total' => ($retur['qty'] ?? 0) * ($retur['unit_price'] ?? 0),
+                'sub_total' => floatval(str_replace(',', '.', $retur['qty'] ?? 0)) * floatval(str_replace(',', '.', $retur['unit_price'] ?? 0)),
             ];
         }
 

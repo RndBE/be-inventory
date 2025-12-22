2ce13375b4f058d7f7993de0ffa023c26cd3288c
RndBE <rnd.software.be@gmail.com>
2025-12-22 12:49:20 +0700

    tablebarangkeluar

--- resources/views/livewire/bahan-keluar-table.blade.php
+++ resources/views/livewire/bahan-keluar-table.blade.php
@@ -140,7 +140,8 @@
                                     {{-- <span class="bg-blue-100 me-2 px-2.5 py-0.5 rounded-full text-blue-800 text-xs font-medium dark:bg-gray-700 dark:text-blue-400 border border-blue-400">{{ $bahan_keluar->status_pengambilan }}</span> --}}
                                     <span class="inline-flex items-center whitespace-nowrap bg-blue-100 px-2.5 py-0.5 rounded-full  text-blue-800 text-xs font-medium dark:bg-gray-700 dark:text-blue-400 border border-blue-400">{{ $bahan_keluar->status_pengambilan }}</span>
                                 @elseif($bahan_keluar->status_pengambilan == 'Sudah Diambil')
-                                    <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-green-400 border border-green-100">{{ $bahan_keluar->status_pengambilan }}</span>
+                                    {{-- <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-green-400 border border-green-100">{{ $bahan_keluar->status_pengambilan }}</span> --}}
+                                    <span class="inline-flex items-center whitespace-nowrap bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-green-400 border border-green-100">{{ $bahan_keluar->status_pengambilan }}</span>
                                 @else
                                     <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-red-400 border border-red-100">{{ $bahan_keluar->status_pengambilan ?? 'Null' }}</span>
                                 @endif
